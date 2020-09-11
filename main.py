552bce1589114c6778ecdb00f858347f7b344041
cusureau <cedric.usureau@gmail.com>
2020-09-11 11:53:18 +0200

    upload data

--- main.py
+++ main.py
@@ -11,9 +11,23 @@ import write_svg
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument("-d", "--mfi", type=str, help="Insert MFI file value (.xls)")
-parser.add_argument("-t", "--template", help="Insert template file", type=str)
-parser.add_argument("-o", "--output", help="output_prefix", type=str)
+parser.add_argument(
+    "-d",
+    "--mfi",
+    type=str,
+    help="Insert MFI file value (.xls)",
+    default="data/sample_example/SA1_ex1.xls",
+)
+parser.add_argument(
+    "-t",
+    "--template",
+    help="Insert template file",
+    type=str,
+    default="data/graph_template/HLA_A_V2.svg",
+)
+parser.add_argument(
+    "-o", "--output", help="output_prefix", type=str, default="result/test/test.svg"
+)
 
 args = parser.parse_args()
 
@@ -24,4 +38,3 @@ data = write_svg.parse_excel_file(args.mfi)
 
 ligne_to_color_in_red = write_svg.node_color_to_change(circle_ligne, data)
 write_svg.rewrite_svg_file(svg_liste, ligne_to_color_in_red, args.output)
-
--- test/main_test.py
+++
@@ -1,5 +0,0 @@
-import main
-
-
-def test_square():
-    assert main.square(2) == 4
