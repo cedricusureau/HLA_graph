69c58c3f443b60e1144b82608f775ef4ce915041
cedricusureau <cedric.usureau@gmail.com>
2020-10-14 16:53:43 +0200

    few template ajdustment

---
+++ src/deletes_files.py
@@ -0,0 +1,11 @@
+import os
+
+def delete_in_folders(path_to_folder, extension):
+    for i in os.listdir(path_to_folder):
+        if extension in i:
+            os.remove(path_to_folder+i)
+
+delete_in_folders("/home/cusureau/HLA_graph/result/graph/", ".svg")
+delete_in_folders("/home/cusureau/HLA_graph/result/html_table/",".html")
+delete_in_folders("/home/cusureau/HLA_graph/result/json/",".json")
+delete_in_folders("/home/cusureau/HLA_graph/result/raw_data/",".csv")
